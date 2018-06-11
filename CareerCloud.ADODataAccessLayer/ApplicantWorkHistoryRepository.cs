1ad538d19f9a643e555362dd54ad2eaeef45c1e0
Lok Prakash Pandey <lokprakashpandey@yahoo.com>
2018-06-11 09:27:29 -0400

    ApplicantWorkHistoryRepo done

--- CareerCloud.ADODataAccessLayer/ApplicantWorkHistoryRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantWorkHistoryRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -9,11 +10,44 @@ using CareerCloud.Pocos;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    public class ApplicantWorkHistoryRepository : IDataRepository<ApplicantWorkHistoryPoco>
+    public class ApplicantWorkHistoryRepository : BaseADO, IDataRepository<ApplicantWorkHistoryPoco>
     {
         public void Add(params ApplicantWorkHistoryPoco[] items)
         {
-            throw new NotImplementedException();
+            using (SqlConnection _connection = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _connection;
+                int rowsEffected = 0;
+                foreach (ApplicantWorkHistoryPoco poco in items)
+                {
+                    cmd.CommandText = @"INSERT INTO APPLICANT_WORK_HISTORY 
+                         (Id,Applicant,Company_Name,Country_Code,
+                          Location,Job_Title,Job_Description,
+                          Start_Month,Start_Year,End_Month,End_Year)
+                         VALUES
+                         (@Id,@Applicant,@Company_Name,@Country_Code,
+                          @Location,@Job_Title,@Job_Description,
+                          @Start_Month,@Start_Year,@End_Month,@End_Year)";
+
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+                    cmd.Parameters.AddWithValue("@Applicant", poco.Applicant);
+                    cmd.Parameters.AddWithValue("@Company_Name", poco.CompanyName);
+                    cmd.Parameters.AddWithValue("@Country_Code", poco.CountryCode);
+                    cmd.Parameters.AddWithValue("@Location", poco.Location);
+                    cmd.Parameters.AddWithValue("@Job_Title", poco.JobTitle);
+                    cmd.Parameters.AddWithValue("@Job_Description", poco.JobDescription);
+                    cmd.Parameters.AddWithValue("@Start_Month", poco.StartMonth);
+                    cmd.Parameters.AddWithValue("@Start_Year", poco.StartYear);
+                    cmd.Parameters.AddWithValue("@End_Month", poco.EndMonth);
+                    cmd.Parameters.AddWithValue("@End_Year", poco.EndYear);
+
+                    _connection.Open();
+                    rowsEffected += cmd.ExecuteNonQuery();
+                    _connection.Close();
+
+                }
+            }
         }
 
         public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
@@ -23,7 +57,44 @@ namespace CareerCloud.ADODataAccessLayer
 
         public IList<ApplicantWorkHistoryPoco> GetAll(params Expression<Func<ApplicantWorkHistoryPoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            ApplicantWorkHistoryPoco[] pocos = new ApplicantWorkHistoryPoco[1000];
+
+            using (SqlConnection _connection = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _connection;
+                cmd.CommandText = "select * from Applicant_Work_History";
+
+                _connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+
+                int position = 0;
+                while (reader.Read())
+                {
+                    ApplicantWorkHistoryPoco poco = new ApplicantWorkHistoryPoco();
+
+                    poco.Id = reader.GetGuid(0);
+                    poco.Applicant = reader.GetGuid(1);
+                    poco.CompanyName = reader.GetString(2);
+                    poco.CountryCode = reader.GetString(3);
+                    poco.Location = reader.GetString(4);
+                    poco.JobTitle = reader.GetString(5);
+                    poco.JobDescription = reader.GetString(6);
+                    poco.StartMonth = reader.GetInt16(7);
+                    poco.StartYear = reader.GetInt32(8);
+                    poco.EndMonth = reader.GetInt16(9);
+                    poco.EndYear = reader.GetInt32(10);
+                    poco.TimeStamp = (byte[])reader[11];
+                    //(byte[])reader["Time_Stamp"]
+                    pocos[position] = poco;
+                    position++;
+
+                }
+                _connection.Close();
+            }
+
+            return pocos.Where(p => p != null).ToList();
         }
 
         public IList<ApplicantWorkHistoryPoco> GetList(Expression<Func<ApplicantWorkHistoryPoco, bool>> where, params Expression<Func<ApplicantWorkHistoryPoco, object>>[] navigationProperties)
@@ -33,17 +104,69 @@ namespace CareerCloud.ADODataAccessLayer
 
         public ApplicantWorkHistoryPoco GetSingle(Expression<Func<ApplicantWorkHistoryPoco, bool>> where, params Expression<Func<ApplicantWorkHistoryPoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            IQueryable<ApplicantWorkHistoryPoco> pocos = GetAll().AsQueryable();
+
+            return pocos.Where(where).FirstOrDefault();
         }
 
         public void Remove(params ApplicantWorkHistoryPoco[] items)
         {
-            throw new NotImplementedException();
+            using (SqlConnection _connection = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _connection;
+                foreach (ApplicantWorkHistoryPoco poco in items)
+                {
+                    cmd.CommandText = @"DELETE FROM Applicant_Work_History
+                                WHERE ID = @Id";
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+
+                    _connection.Open();
+                    cmd.ExecuteNonQuery();
+                    _connection.Close();
+                }
+            }
         }
 
         public void Update(params ApplicantWorkHistoryPoco[] items)
         {
-            throw new NotImplementedException();
+            using (SqlConnection _connection = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _connection;
+                foreach (ApplicantWorkHistoryPoco poco in items)
+                {
+                    cmd.CommandText = @"UPDATE Applicant_Work_History
+                            SET Applicant = @Applicant,
+                                Company_Name = @Company_Name,
+                                Country_Code = @Country_Code,
+                                Location = @Location,
+                                Job_Title = @Job_Title,
+                                Job_Description = @Job_Description,
+                                Start_Month = @Start_Month,
+                                Start_Year = @Start_Year,
+                                End_Month = @End_Month,
+                                End_Year = @End_Year
+                                WHERE ID = @Id";
+
+                    cmd.Parameters.AddWithValue("@Applicant", poco.Applicant);
+                    cmd.Parameters.AddWithValue("@Company_Name", poco.CompanyName);
+                    cmd.Parameters.AddWithValue("@Country_Code", poco.CountryCode);
+                    cmd.Parameters.AddWithValue("@Location", poco.Location);
+                    cmd.Parameters.AddWithValue("@Job_Title", poco.JobTitle);
+                    cmd.Parameters.AddWithValue("@Job_Description", poco.JobDescription);
+                    cmd.Parameters.AddWithValue("@Start_Month", poco.StartMonth);
+                    cmd.Parameters.AddWithValue("@Start_Year", poco.StartYear);
+                    cmd.Parameters.AddWithValue("@End_Month", poco.EndMonth);
+                    cmd.Parameters.AddWithValue("@End_Year", poco.EndYear);
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+
+                    _connection.Open();
+                    int rowEffected = cmd.ExecuteNonQuery();
+                    _connection.Close();
+
+                }
+            }
         }
     }
 }
