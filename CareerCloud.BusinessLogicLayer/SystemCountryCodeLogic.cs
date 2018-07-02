ed3b7011c53a141d21b665602bf536d2c3992995
Lok Prakash Pandey <lokprakashpandey@yahoo.com>
2018-07-02 10:43:14 -0400

    Upto no 5 point

---
+++ CareerCloud.BusinessLogicLayer/SystemCountryCodeLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class SystemCountryCodeLogic
+    {
+ 
+    }
+}
--- CareerCloud.BusinessLogicLayer/SystemLanguageCodeLogic.cs
+++ CareerCloud.BusinessLogicLayer/SystemLanguageCodeLogic.cs
@@ -4,11 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CareerCloud.Pocos
+namespace CareerCloud.BusinessLogicLayer
 {
-public interface IPoco
+    public class SystemLanguageCodeLogic
     {
-        Guid Id { get; set; }
-
     }
 }
