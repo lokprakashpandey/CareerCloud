20c6a8a76509c417714f9db6b6c101f88bdaba61
Lok Prakash Pandey <lokprakashpandey@yahoo.com>
2018-09-15 22:35:10 -0400

    Company and Applicant Controllers done

--- CareerCloud.WebAPI/Controllers/CompanyProfileController.cs
+++ CareerCloud.WebAPI/Controllers/CompanyProfileController.cs
@@ -4,10 +4,80 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
 
 namespace CareerCloud.WebAPI.Controllers
 {
+    [RoutePrefix("api/careercloud/company/v1")]
     public class CompanyProfileController : ApiController
     {
+        private CompanyProfileLogic _logic;
+
+        public CompanyProfileController()
+        {
+            var repo =
+                new EFGenericRepository<CompanyProfilePoco>(false);
+
+            _logic = new CompanyProfileLogic(repo);
+        }
+
+        [HttpGet]
+        [Route("profile/{companyProfileId}")]
+        [ResponseType(typeof(CompanyProfilePoco))]
+        public IHttpActionResult GetCompanyJob(Guid companyProfileId)
+        {
+            CompanyProfilePoco poco = _logic.Get(companyProfileId);
+
+            if (poco == null)
+            {
+                return NotFound();
+            }
+            return Ok(poco);
+        }
+
+        [HttpGet]
+        [Route("profile")]
+        [ResponseType(typeof(List<CompanyProfilePoco>))]
+        public IHttpActionResult GetAllCompanyProfile()
+        {
+            List<CompanyProfilePoco> result = _logic.GetAll();
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("profile")]
+        public IHttpActionResult PostCompanyProfile
+            ([FromBody] CompanyProfilePoco[] pocos)
+        {
+            _logic.Add(pocos);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("profile")]
+        public IHttpActionResult PutCompanyProfile
+            ([FromBody] CompanyProfilePoco[] pocos)
+        {
+            _logic.Update(pocos);
+            return Ok();
+        }
+
+
+        [HttpDelete]
+        [Route("profile")]
+        public IHttpActionResult DeleteCompanyProfile
+            ([FromBody] CompanyProfilePoco[] pocos)
+        {
+            _logic.Delete(pocos);
+            return Ok();
+        }
     }
 }
